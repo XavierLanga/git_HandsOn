d6ce07b8cbad96eec8dcb6a93cf0b28cfbdfb39f
XavierLanga <xavilanol@gmail.com>
2025-03-15 16:35:41 +0100

    Refactor: Improve code readability and formatting

--- seqClass.py
+++ seqClass.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python
 
-import sys, re
+import sys
+import re
 from argparse import ArgumentParser
 
-parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
-parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
+# Initialize argument parser
+parser = ArgumentParser(description='Classify a sequence as DNA or RNA')
+parser.add_argument("-s", "--seq", type=str, required=True, help="Input sequence")
 
+# If no arguments are provided, print help message and exit
 if len(sys.argv) == 1:
     parser.print_help()
     sys.exit(1)
 
+# Parse arguments
 args = parser.parse_args()
 
 # Convert sequence to uppercase to handle lowercase input
-seq = args.seq.upper()
+args.seq = args.seq.upper()
 
-if re.search('^[ACGTU]+$', seq):
-    if re.search('T', seq):
-        print ('The sequence is DNA')
-    elif re.search('U', seq):
-        print ('The sequence is RNA')
+# Check if the sequence contains only valid DNA/RNA bases
+if re.search('^[ACGTU]+$', args.seq):
+    if 'T' in args.seq:
+        print('The sequence is DNA')
+    elif 'U' in args.seq:
+        print('The sequence is RNA')
     else:
-        print ('The sequence can be DNA or RNA')
+        print('The sequence can be DNA or RNA')
 else:
-    print ('The sequence is not DNA nor RNA')
+    print('The sequence is not DNA nor RNA')
+
